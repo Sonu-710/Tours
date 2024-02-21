c9d8135c1df94c675c26ae9d3461f860b92c2f45
Sonu Acharya <sonuacharya2022@gmail.com>
2024-02-21 14:21:55 +0530

    Sending email using nodemailer implemented

--- starter/utils/email.js
+++ starter/utils/email.js
@@ -1,11 +1,27 @@
-const nodemailer=require('nodemailer');
-const sendEmail=options =>
-{
-    // 1 --> Create Transpoter
-    const trasporter= nodemailer.createTransport({
-        
-    })
-    // 2 --> Define the email options
+const nodemailer = require('nodemailer');
 
-    // 3 --> Actually send the email
-}
+const sendEmail = async (options) => {
+  // 1 --> Create Transpoter
+  const trasporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSOWRD,
+    },
+  });
+
+  // 2 --> Define the email options
+  const mailOptions = {
+    from: 'Sonu Acharya <sonuacharya710@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  // 3 --> Actually send the email
+  await trasporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
