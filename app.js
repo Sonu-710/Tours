127082aa5e07cd5ecb8d040d5cf5fdf2ab17194a
Sonu Acharya <sonuacharya2022@gmail.com>
2024-03-17 15:53:44 +0530

    Enhanced the security

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
 const morgan = require(`morgan`);
 const express = require('express');
-const app = express();
 const appError = require('./starter/utils/AppError');
 const globalErrorHandler = require('./starter/controller/errorController');
 const toursRouter = require('./starter/router/tours');
 const userRouter = require('./starter/router/users');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitze = require('express-mongo-sanitize');
+const hpp = require('hpp');
 
+const app = express();
 //GLOBAL MIDDLEWARE
 app.use(morgan('dev'));
-app.use(express.json());
+//Add Headers to make it secure
+app.use(helmet());
+app.use(express.json({ limit: '10kb' }));
 app.use(express.static(`./starter/public`));
 // app.use((req, res, next) => {
 //   console.log("Hello from the middleware");
 //   next();
 // });
 
+//Limit the max times access
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP,please try again in an Hour!!',
 });
-
 app.use('/api', limiter);
+
+//Data Sanitization against NOSQL query injection
+app.use(mongoSanitze());
+
+//Data Sanitization against
+app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingAverage',
+      'ratingQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
 app.use((req, res, next) => {
   req.time = new Date();
   next();
