958c5d0371a073520a24ba0106cbc26babe52f14
Sonu Acharya <sonuacharya2022@gmail.com>
2024-03-17 14:39:48 +0530

    Update Current User Route Implemented

--- starter/controller/userController.js
+++ starter/controller/userController.js
@@ -1,7 +1,16 @@
 const User = require('./../Models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/AppError');
 
-exports.getAllUsers = catchAsync(async(req, res) => {
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
 
   //SEND RESPONSE
@@ -39,3 +48,30 @@ exports.deleteUser = (req, res) => {
     message: 'Route is yet to be defined!!!',
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1 --> Create error if user Posts Password data
+  if (req.body.password || req.body.confrim_password) {
+    return next(
+      new AppError(
+        'This route is not for password updates.Please use /updateMyPassword',
+        400,
+      ),
+    );
+  }
+
+  // 2--> Filter the fields allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  // 3--> Upadte user Document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
