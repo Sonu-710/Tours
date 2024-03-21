2012b7d2f69b2b8b4b529f599f7ffa5c3c45eb9e
Sonu Acharya <sonuacharya2022@gmail.com>
2024-03-21 15:28:10 +0530

    Added Indexes to prevent dublicate reviews

--- starter/Models/reviewModel.js
+++ starter/Models/reviewModel.js
@@ -33,6 +33,8 @@ const reviewSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   },
 );
+
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
   //   path: 'tour',
@@ -61,8 +63,8 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
   console.log(stats);
   if (stats.length > 0) {
     await Tour.findByIdAndUpdate(tourId, {
-      ratingAverage: stats[0].nRating,
-      ratingQuantity: stats[0].avgRating,
+      ratingAverage: stats[0].avgRating,
+      ratingQuantity: stats[0].nRating,
     });
   } else {
     await Tour.findByIdAndUpdate(tourId, {
