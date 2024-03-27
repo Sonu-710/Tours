0779755f1f3f34a56228059aff8be4d361249e6c
Sonu Acharya <sonuacharya2022@gmail.com>
2024-03-27 16:31:18 +0530

    Fixed issue with the heading

--- starter/controller/viewsController.js
+++ starter/controller/viewsController.js
@@ -19,7 +19,7 @@ exports.getTour = catchAsync(async (req, res) => {
     fields: 'review rating user',
   });
   res.status(200).render('tours', {
-    title: 'The Forest Hiker',
+    title: `${tour.name} tour`,
     tour
   });
 });
