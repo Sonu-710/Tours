ffb081693d93bec6b0e60421290c5a1d4429db1a
Sonu Acharya <sonuacharya2022@gmail.com>
2024-03-29 12:59:58 +0530

    Added CSS to the Alerts

--- starter/public/js/login.js
+++ starter/public/js/login.js
@@ -10,23 +10,35 @@ const login = async (email, password) => {
     });
 
     if (res.data.status == 'success') {
-      alert('Logged in successfully!!');
+      showAlert('success','Logged in successfully!!');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (err) {
     console.error(err);
-    alert(err.response.data.message);
+    showAlert('error',err.response.data.message);
   }
 };
 
-const loginForm=document.querySelector('.form');
-if(loginForm){
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
+const loginForm = document.querySelector('.form');
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
 }
+
+const hideAlert = () => {
+  const el = document.querySelector('.alert');
+  if (el) el.parentElement.removeChild(el);
+};
+
+const showAlert = (type, msg) => {
+  hideAlert();
+  const markup = `<div class="alert alert--${type}">${msg}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+  window.setTimeout(hideAlert, 5000);
+};
